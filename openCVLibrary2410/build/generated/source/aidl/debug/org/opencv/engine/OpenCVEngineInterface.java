ac8bcc45e9db16ff663074ef5c8f2dbd552427f5
Evan Bowman <evanbowman@wireless1x-155-41-126-36.bu.edu>
2015-10-06 19:24:46 -0400

    The finished version.

--- openCVLibrary2410/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary2410/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/evanbowman/Downloads/BluePencil-master/openCVLibrary2410/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: /Users/evanbowman/Documents/College/Software Engineering/App Project/BluePencil-master/openCVLibrary2410/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
